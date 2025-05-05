c26992897cfe3260f43f054bd105b64b83673fe4
Sena <matheussena901@gmail.com>
2025-05-05 11:53:24 -0400

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,12 +1,16 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		Department obj = new Department(1, "Books");
-		System.out.println(obj);
+		Seller seller = new Seller(2, "sena", "matheussena901@gmail.com", new Date(), 8000.0, obj);
+		System.out.println(seller);
 
 	}
 
